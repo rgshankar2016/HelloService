8e8f66b8afafb21687a262ae10607d8efab02b2c
rgshankar <rgshankar2016@gmail.com>
2016-03-08 16:36:53 +0100

    first commit

---
+++ src/com/ibm/rest/MyApplication.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.ibm.rest;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+/**
+ * @author raman
+ *
+ */
+public class MyApplication extends Application{
+	
+	 @Override
+	    public Set<Class<?>> getClasses() {
+	        Set<Class<?>> s = new HashSet<Class<?>>();
+
+	        s.add(CtoFService.class);
+	  
+	        return s;
+	    }
+
+}
